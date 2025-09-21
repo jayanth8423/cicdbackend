5d57b326dbddcb7fb2200e46428856ce8ef21428
jayanth8423 <163724366+jayanth8423@users.noreply.github.com>
2025-09-21 15:12:59 +0530

    Update ProjectApplicationTests.java

--- src/test/java/klu/ProjectApplicationTests.java
+++ src/test/java/klu/ProjectApplicationTests.java
@@ -3,11 +3,10 @@ package klu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjectApplication.class)
 class ProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
