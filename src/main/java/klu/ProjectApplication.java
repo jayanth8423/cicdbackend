2be7c38190aae1f34f2a5abbd1ecb159b6e3c47c
jayanth8423 <163724366+jayanth8423@users.noreply.github.com>
2025-09-21 15:02:50 +0530

    Update ProjectApplication.java

--- src/main/java/klu/ProjectApplication.java
+++ src/main/java/klu/ProjectApplication.java
@@ -1,13 +1,12 @@
 package klu;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class ProjectApplication {
+@SpringBootTest(classes = ProjectApplication.class) // specify main class
+class ProjectApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
